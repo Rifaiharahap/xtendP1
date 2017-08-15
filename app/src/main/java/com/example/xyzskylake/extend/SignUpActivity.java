298debed4c8d5b46269cdac92fa3b3db52038bd5
Ali <you@example.com>
2017-08-15 15:31:55 +0700

    Change name java class and activty class

--- app/src/main/java/com/example/xyzskylake/extend/SignUpActivity.java
+++ app/src/main/java/com/example/xyzskylake/extend/SignUpActivity.java
@@ -3,7 +3,7 @@ package com.example.xyzskylake.extend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main4Activity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
